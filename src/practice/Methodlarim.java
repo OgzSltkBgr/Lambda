5711abaa54dde238e4fd160c24c8e5777c65e613
OgzSltkBgr <osbtoklucu@gmail.com>
2022-08-23 16:48:02 +0300

    2308

---
+++ src/practice/Methodlarim.java
@@ -0,0 +1,8 @@
+package practice;
+
+public class Methodlarim {
+    public static boolean ciftElemaniBul(int x){
+        return  x % 2==0;
+        // 2.yol return x % 2!=1;
+    }
+}
---
+++ src/practice/Runner01.java
@@ -0,0 +1,21 @@
+package practice;
+
+public class Runner01 extends Lambda01{
+    public static void main(String[] args) {
+        // aHarfleriSilenGerisiniYazdiran(names);
+        System.out.println("**************");
+        //  maxElemanBul(numbers);
+        System.out.println("**************");
+        //  elemanlariTopla(numbers);
+        System.out.println("**************");
+        elemanlariCarp(numbers);
+        System.out.println("**************");
+
+
+
+
+
+
+
+    }
+}
